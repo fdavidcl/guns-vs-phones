0d5008dc60acfe8af7606212dd0879ab192b4f64
David Charte <fdavidcl@protonmail.com>
2018-03-12 19:19:46 +0100

    all

--- cnn.py
+++ cnn.py
@@ -1,6 +1,72 @@
 #!/usr/bin/env python
 
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 from keras.layers import Conv2D, MaxPooling2D, Dense, Reshape, Flatten, Input
 from keras.models import Sequential
 from keras.utils.np_utils import to_categorical
 import numpy as np
+from read_data import read_data
+
+### MODELO
+def cnn():
+    model = Sequential()
+
+    #model.add(Reshape((28, 28, 1), input_shape = (28, 28)))
+    model.add(Conv2D(
+        input_shape = (160, 120, 3)
+        , filters = 32
+        , kernel_size = (3, 3)
+        , padding = "same"
+        , activation = "relu"
+    ))
+    model.add(MaxPooling2D())
+    model.add(Conv2D(
+        filters = 64
+        , kernel_size = (3, 3)
+        , padding = "same"
+        , activation = "relu"
+    ))
+    model.add(MaxPooling2D())
+    model.add(Conv2D(
+        filters = 128
+        , kernel_size = (3, 3)
+        , padding = "same"
+        , activation = "relu"
+    ))
+    model.add(Flatten())
+    model.add(Dense(2048))
+    model.add(Dense(100))
+    model.add(Dense(2, activation = "softmax"))
+
+    return model
+
+def dense():
+    model = Sequential()
+
+#### TRAIN
+x_train, y_train, x_test = read_data()
+        
+# normalize
+#x_train = x_train.astype('float32') / 255.
+#x_test = x_test.astype('float32') / 255.
+
+y_train = to_categorical(y_train, num_classes = 2)
+#y_test = to_categorical(y_test, num_classes = 10)
+
+print(x_train.shape)
+print(y_train.shape)
+
+model = cnn()
+
+model.compile("rmsprop", "categorical_crossentropy")
+model.fit(
+    x_train, y_train,
+    batch_size = 32,
+    epochs = 2,
+    shuffle = True
+)
+
+preds = model.predict(x_test)
+print(preds)
---
+++ read_data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import numpy as np
+from PIL import Image
+import glob, os
+
+from keras.preprocessing import image
+
+def get_imgs(img_list):
+    img = [image.load_img(filename, target_size = (160, 120), interpolation = "bilinear") for filename in img_list]
+    print(len(img))
+    npi = [np.array(i) for i in img]
+    return np.stack(npi, axis = 0)    
+        
+
+def read_data():
+    train_x_pistol = get_imgs(glob.glob("Train/Pistol/*.jpg"))
+    train_x_phone = get_imgs(glob.glob("Train/Smartphone/*.jpg"))
+
+    train_x = np.concatenate((train_x_pistol, train_x_phone), axis = 0)
+    train_y = np.concatenate((np.zeros(train_x_pistol.shape[0]), np.ones(train_x_phone.shape[0])))
+    
+    test_x = get_imgs(glob.glob("Test/*.jpg"))
+
+    return (train_x, train_y, test_x)
+
