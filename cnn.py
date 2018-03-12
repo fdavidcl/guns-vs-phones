ea21f24bd8cdbcd28f17c1ce2d0f313f737964ff
David Charte <fdavidcl@protonmail.com>
2018-03-12 17:58:27 +0100

    Add imports

---
+++ cnn.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from keras.layers import Conv2D, MaxPooling2D, Dense, Reshape, Flatten, Input
+from keras.models import Sequential
+from keras.utils.np_utils import to_categorical
+import numpy as np
